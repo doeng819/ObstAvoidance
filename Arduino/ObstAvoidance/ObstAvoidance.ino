894cbb769b6a8b3f3244c0b86ad7216412ed96f1
doeng819 <doeng819@icloud.com>
2015-03-22 20:49:27 -0700

    first algorithm commit

---
+++ Arduino/ObstAvoidance/ObstAvoidance.ino
@@ -0,0 +1,154 @@
+
+#include "Arduino.h"
+#include <Servo.h>
+
+//////////////////////////////////////////
+/// ultrasonic sensor
+//////////////////////////////////////////
+
+class Ultrasonic
+{
+  public:
+    Ultrasonic(int pin);
+    void DistanceMeasure(void);
+    long microsecondsToCentimeters(void);
+  private:
+    int _pin;//pin number of Arduino that is connected with SIG pin of Ultrasonic Ranger.
+    long duration;// the Pulse time received;
+};
+Ultrasonic::Ultrasonic(int pin)
+{
+  _pin = pin;
+}
+/*Begin the detection and get the pulse back signal*/
+void Ultrasonic::DistanceMeasure(void)
+{
+  pinMode(_pin, OUTPUT);
+  digitalWrite(_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(_pin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(_pin, LOW);
+  pinMode(_pin, INPUT);
+  duration = pulseIn(_pin, HIGH);
+}
+/*The measured distance from the range 0 to 400 Centimeters*/
+long Ultrasonic::microsecondsToCentimeters(void)
+{
+  return duration / 29 / 2;
+}
+
+//VH_RADIUS 30 //回転半径30cm
+//sqrt(2)*VH_RADIUS = 42.4264068712cm
+const long COLLISION_THRES = 42;
+//ultrasonic instance declared before use
+Ultrasonic ultrasonic(7);
+
+boolean Sensor_Detect_Obstacle(void)
+{
+  long RangeInCentimeters;
+
+  ultrasonic.DistanceMeasure();// get the current signal time;
+  RangeInCentimeters = ultrasonic.microsecondsToCentimeters();//convert the time to centimeters
+
+  Serial.print("Sensor_Detect_Obstacle :");
+  Serial.println(RangeInCentimeters);
+
+  if (RangeInCentimeters <= COLLISION_THRES)
+    return true;
+  else
+    return false;
+}
+
+//////////////////////////////////////////
+/// servo
+//////////////////////////////////////////
+
+Servo myservo;  // create servo object to control a servo
+// a maximum of eight servo objects can be created
+
+//CurPos 0:  0 degrees (left)
+//CurPos 1: 45 degrees
+//CurPos 2: 90 degrees (center)
+//CurPos 3: 135 degrees
+//CurPos 4: 180 degrees (right)
+const int CurPosMax = 5;
+const int CenterPos = 2;
+const int CurPosArray[] = {0,45,90,135,180};
+
+void Sensor_TurnToPos(int CurPos)
+{
+  myservo.write(CurPosArray[CurPos]);          // tell servo to go to position in variable 'pos'
+  delay(1500);                       // waits for the servo to reach the position
+
+  Serial.print("Sensor_TurnToPos :");
+  Serial.println(CurPos);
+}
+
+void Vehicle_ResumeMove(void)
+{
+  Serial.println("Vehicle_ResumeMove :");
+}
+
+void Vehicle_StopMove(void)
+{
+  Serial.println("Vehicle_StopMove :");
+}
+
+void Vehicle_TurnToPos(int CurPos)
+{
+  Serial.print("Vehicle_TurnToPos :");
+  Serial.println(CurPos);
+
+}
+
+void setup()
+{
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(9600);
+}
+
+
+void loop()
+{
+  boolean ObstacleFlag;
+  int CurPos;
+
+  ObstacleFlag = Sensor_Detect_Obstacle();
+
+  if (ObstacleFlag == true)
+  {
+    Vehicle_StopMove();
+    CurPos = 0;
+
+    while (CurPos < CurPosMax)
+    {
+      Sensor_TurnToPos(CurPos);
+      ObstacleFlag = Sensor_Detect_Obstacle();
+
+      if (ObstacleFlag == false)
+      {
+        //found a way out
+        Sensor_TurnToPos(CenterPos);
+        Vehicle_TurnToPos(CurPos);
+        CurPos = CurPosMax; //in order to exit from the loop
+      }
+      else
+      {
+        //still obstacle exists
+        CurPos++;
+      }
+    }
+
+    //Sweeped right and left but still obstacles all around
+    if (ObstacleFlag == true)
+    {
+      Serial.println("Vehicle will turn 180 and exit :");
+      Sensor_TurnToPos(CenterPos);
+      Vehicle_TurnToPos(180);
+      Vehicle_ResumeMove();
+    }
+  }
+
+}
+
--- Arduino/servo/servo.ino
+++ Arduino/servo/servo.ino
@@ -20,15 +20,33 @@ void setup()
  
 void loop() 
 { 
+  
+   myservo.write(0);              // tell servo to go to position in variable 'pos' 
+   delay(1500);                       // waits 15ms for the servo to reach the position 
+
+    myservo.write(45);              // tell servo to go to position in variable 'pos' 
+    delay(1500);                       // waits 15ms for the servo to reach the position 
+
+    myservo.write(90);              // tell servo to go to position in variable 'pos' 
+    delay(1500);                       // waits 15ms for the servo to reach the position 
+
+    myservo.write(135);              // tell servo to go to position in variable 'pos' 
+    delay(1500);                       // waits 15ms for the servo to reach the position 
+
+    myservo.write(180);              // tell servo to go to position in variable 'pos' 
+    delay(1500);                       // waits 15ms for the servo to reach the position 
+/*  
   for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
   {                                  // in steps of 1 degree 
     myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(5);                       // waits 15ms for the servo to reach the position 
+    delay(15);                       // waits 15ms for the servo to reach the position 
   } 
-//   delay(1000);                       // waits 15ms for the servo to reach the position 
-  for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
-  {                                
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(5);                       // waits 15ms for the servo to reach the position 
-  } 
+   delay(1000);                       // waits 15ms for the servo to reach the position 
+//  for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
+//  {                                
+//    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+//    delay(5);                       // waits 15ms for the servo to reach the position 
+//  } 
+  */
+  delay(1000);
 }
